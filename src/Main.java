80402d15397eace65a3ccc8e9ce002ce4f7cd90e
ALEX TEODORESCU <alex.teodorescu16@gmail.com>
2025-08-01 14:18:43 +0300

    Tema7 - Teodorescu Alex

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("Alex Teodorescu");
+
+        int x = 2;
+        int y = 3;
+        System.out.println("X + Y= " +(x + y));
+
+        float a = 2;
+        float b = 3;
+        System.out.println("A / B = " +(a / b));
+
+        x = -5 + 8 * 6;
+        System.out.println("Exercitiu a. = " +x);
+        x = (55+9) % 9;
+        System.out.println("Exercitiu b. = " +x);
+        x = 20 + -3*5 / 8;
+        System.out.println("Exercitiu c. = " +x);
+        x = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println("Exercitiu d. = " +x);
+
+
+
+    }
+}
